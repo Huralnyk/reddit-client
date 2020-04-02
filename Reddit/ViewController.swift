f31f34e261ccd085cbc3a3912f806c591520abe0
Oleksii Huralnyk <alexey.huralnyk@gmail.com>
2020-04-02 10:20:19 +0300

    Initial Commit

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Oleksii Huralnyk on 02.04.2020.
+//  Copyright © 2020 Oleksii Huralnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
